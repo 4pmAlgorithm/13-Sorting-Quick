030cdadf6d48844c25bd8faeb31eaf30db035a67
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-15 13:38:31 -0500

    quick

---
+++ quickSort.js
@@ -0,0 +1,11 @@
+// 1. It will help to accept three arguments: an array, a start index, and an end index(these can default to 0 and the array length minus1, respectively)
+// 2. Grab the pibot from the start of the array. 
+// 3. Store the current pivot index in a variable(this will keep track of where the pivot should end up)
+// 4. Loop through the array from the start until the end
+//     4-1. If the pivot is greater than the current element, increment the pibot index variable and then swap the current element with the element at the pivot index
+// 5. swap the starting element (i.e. the pivot) with the pivot index
+// 6. return the pivot index
+
+
+
+
